f4ec12aab03465b096c5868c2555f04f11db32bb
Ezii54 <alunheadshoter@gmail.com>
2025-05-09 16:20:07 +0700

    method download

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,40 @@
 "use client";
 
+import { DownloadSimple } from "@phosphor-icons/react";
 import { useRef, useState } from "react";
+import "./globals.css";
 
 export default function Home() {
     const [text, setText] = useState("");
-    const [copy, setCopy] = useState(false);
+    const [filename, setFilename] = useState("");
+    const [downloaded, setDownloaded] = useState(false);
 
     const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-    const handleCopy = async () => {
+    const charCount = text.replace(/\s+/g, "").length;
+    const wordCount = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+
+    const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+        if (downloaded) setDownloaded(false);
+    };
+
+    const handleDownload = async () => {
         if (text.trim() === "") return;
+        if (filename.trim() === "") return;
         try {
-            const textArea = document.createElement("textarea");
-            textArea.value = text;
-            document.body.appendChild(textArea);
-            textArea.select();
-            document.execCommand("copy");
-            document.body.removeChild(textArea);
-            setCopy(true);
+            const blob = new Blob([text], { type: "text/plain" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = `${filename}.txt`;
+            a.click();
+            URL.revokeObjectURL(url);
+            setDownloaded(true);
         } catch {
             alert("Failed!");
         }
     };
-
-    const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setText(e.target.value);
-        if (copy) setCopy(false);
-    };
-
-    const charCount = text.replace(/\s+/g, "").length;
-    const wordCount = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
 
     return (
         <div className="flex flex-col gap-5 p-5">
@@ -42,24 +47,31 @@ export default function Home() {
                 ref={textareaRef}
                 onChange={handleTextChange}
                 placeholder="type here..."
-                className="w-full p-2 overflow-auto text-lg text-black bg-gray-300 border rounded-lg shadow-inner outline-none shadow-black h-100"
+                className="w-full p-2 overflow-y-scroll text-lg text-black bg-gray-300 border rounded-lg shadow-inner outline-none resize-none scroll-ml-52 shadow-black h-100"
             />
 
-            <div className="flex flex-row justify-between p-2 bg-gray-900 rounded-lg shadow-inner shadow-black">
-                <div className="flex flex-col gap-2 font-mono">
-                    <p>Char Count: {charCount}</p>
-                    <p>Word Count: {wordCount}</p>
+            <div className="flex flex-col justify-between gap-2 p-2 bg-gray-900 rounded-lg shadow-inner shadow-black">
+                <div className="flex flex-col font-mono">
+                    <p className="">Char Count: {charCount}</p>
+                    <p className="">Word Count: {wordCount}</p>
                 </div>
-                <button
-                    onClick={handleCopy}
-                    className={`rounded-lg px-5 transition-all shadow-md shadow-black ${
-                        copy
-                            ? "bg-green-600 hover:bg-green-700 text-white"
-                            : "bg-blue-600 hover:bg-blue-700 text-white"
-                    }`}
-                >
-                    {copy ? "Success" : "Copy All"}
-                </button>
+
+                <div className="flex flex-row gap-2">
+                    <input
+                        type="text"
+                        value={filename}
+                        onChange={(e) => setFilename(e.target.value)}
+                        className="w-full px-2 text-lg text-black bg-gray-300 rounded-md shadow-md outline-none shadow-black"
+                        placeholder="name a file"
+                    />
+                    <button onClick={handleDownload}>
+                        <DownloadSimple
+                            size={40}
+                            weight="light"
+                            className="text-black bg-blue-500 rounded-md shadow-md shadow-black"
+                        />
+                    </button>
+                </div>
             </div>
         </div>
     );
